21d64f4bb2952756d6563a5f46014f9a3a577042
BuildTools <unconfigured@null.spigotmc.org>
2024-02-25 13:16:35 +0900

    微調整

--- server.js
+++ server.js
@@ -82,71 +82,144 @@ app.post('/post.html', upload.single('file'), async function (request, response)
         "gif": "gif",
     };
 
-    async function convertAndSendFile(extension, quality, width, height, resize) {
+    async function convertAndSendFile(extension, quality, width, height, resize, rotates, hanten, yokohanten,boka) {
         const mimeType = mimeTypes[extension];
         let outputBuffer;
         let output2Buffer;
+        let bokasi;
+       
+        if(width>=5000){
+            width = width/10
+        }
+        if(height>=5000){
+            height = height/10
+        }
+        if(boka <= 0.3 || boka >= 100){
+            bokasi = null;
+        }else{
+            bokasi = boka;
+        }
+        console.log(bokasi)
         if (resize == "true") {
 
         } else {
             width = null;
             height = null;
         }
-        const result = await fileType.fromBuffer(request.file.buffer);
-        if (result.ext === 'ico') {
-            output2Buffer = await new Promise((resolve, reject) => {
-                favicon(request.file.buffer, (err, favicons) => {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        // faviconsは、ICOファイル内の各画像のPNGバージョンを含む配列です。
-                        // ここでは最初の画像をバッファとして取得します。
-                        const firstImage = favicons[0];
-                        const buffer = Buffer.from(firstImage.data, 'base64');
-                        resolve(buffer);
-                    }
-                });
-            });
-        } else {
-            output2Buffer = request.file.buffer;
-        }
+        output2Buffer = request.file.buffer;
         if (extension == "jpg") {
+
             if (quality <= 100 && quality >= 1) {
-                outputBuffer = await sharp(output2Buffer).jpeg({
-                    quality: quality,
-                    progressive: true
-                }).resize(width, height).toBuffer();
+                if (hanten == "true") {
+                    if (yokohanten == "true") {
+                        outputBuffer = await sharp(output2Buffer).jpeg({
+                            quality: quality,
+                            progressive: true
+                        }).resize(width, height).rotate(rotates).flip().flop().blur(bokasi).toBuffer();
+                    } else {
+                        outputBuffer = await sharp(output2Buffer).jpeg({
+                            quality: quality,
+                            progressive: true
+                        }).resize(width, height).rotate(rotates).flip().blur(bokasi).toBuffer();
+                    }
+                } else {
+                    if (yokohanten == "true") {
+                        outputBuffer = await sharp(output2Buffer).jpeg({
+                            quality: quality,
+                            progressive: true
+                        }).resize(width, height).rotate(rotates).flop().blur(bokasi).toBuffer();
+                    } else {
+                        outputBuffer = await sharp(output2Buffer).jpeg({
+                            quality: quality,
+                            progressive: true
+                        }).resize(width, height).rotate(rotates).blur(bokasi).toBuffer();
+                    }
+                }
             }
         } else if (extension == "png") {
             if (quality <= 9 && quality >= 1) {
-                outputBuffer = await sharp(output2Buffer).png({
-                    compressionLevel: quality,
-                    progressive: true
-                }).resize(width, height).toBuffer();
+                if (hanten == "true") {
+                    if (yokohanten == "true") {
+                        outputBuffer = await sharp(output2Buffer).png({
+                            compressionLevel: quality,
+                            progressive: true
+                        }).resize(width, height).rotate(rotates).blur(bokasi).flip().flop().toBuffer();
+                    } else {
+                        outputBuffer = await sharp(output2Buffer).png({
+                            compressionLevel: quality,
+                            progressive: true
+                        }).resize(width, height).rotate(rotates).blur(bokasi).flip().toBuffer();
+                    }
+                } else {
+                    if (yokohanten == "true") {
+                        outputBuffer = await sharp(output2Buffer).png({
+                            compressionLevel: quality,
+                            progressive: true
+                        }).resize(width, height).rotate(rotates).blur(bokasi).flop().toBuffer();
+                    } else {
+                        outputBuffer = await sharp(output2Buffer).png({
+                            compressionLevel: quality,
+                            progressive: true
+                        }).resize(width, height).rotate(rotates).blur(bokasi).toBuffer();
+                    }
+                }
             }
         } else if (extension == "webp") {
             if (quality <= 100 && quality >= 1) {
-                outputBuffer = await sharp(output2Buffer).webp({
-                    quality: quality
-                }).resize(width, height).toBuffer();
+                if (hanten == "true") {
+                    if (yokohanten == "true") {
+                        outputBuffer = await sharp(output2Buffer).webp({
+                            quality: quality
+                        }).resize(width, height).rotate(rotates).blur(bokasi).flip().flop().toBuffer();
+                    } else {
+                        outputBuffer = await sharp(output2Buffer).webp({
+                            quality: quality
+                        }).resize(width, height).rotate(rotates).blur(bokasi).flip().toBuffer();
+                    }
+                } else {
+                    if (yokohanten == "true") {
+                        outputBuffer = await sharp(output2Buffer).webp({
+                            quality: quality
+                        }).resize(width, height).rotate(rotates).blur(bokasi).flop().toBuffer();
+                    } else {
+                        outputBuffer = await sharp(output2Buffer).webp({
+                            quality: quality
+                        }).resize(width, height).rotate(rotates).blur(bokasi).toBuffer();
+                    }
+                }
             }
         } else if (extension == "tiff") {
             if (quality <= 100 && quality >= 1) {
-                outputBuffer = await sharp(output2Buffer).tiff({
-                    quality: quality
-                }).resize(width, height).toBuffer();
+                if (hanten == "true") {
+                    if (yokohanten == "true") {
+                    outputBuffer = await sharp(output2Buffer).tiff({
+                        quality: quality
+                    }).resize(width, height).rotate(rotates).blur(bokasi).flip().flop().toBuffer();
+                }else{
+                    outputBuffer = await sharp(output2Buffer).tiff({
+                        quality: quality
+                    }).resize(width, height).rotate(rotates).blur(bokasi).flip().toBuffer();
+                }
+                } else {
+                    if (yokohanten == "true") {
+                    outputBuffer = await sharp(output2Buffer).tiff({
+                        quality: quality
+                    }).resize(width, height).rotate(rotates).blur(bokasi).flop().toBuffer();
+                }else{
+                    outputBuffer = await sharp(output2Buffer).tiff({
+                        quality: quality
+                    }).resize(width, height).rotate(rotates).blur(bokasi).toBuffer();
+                }
+                }
             }
-        } else if (extension == "ico") {
-            convertImage2(request.file.buffer, "ICON")
-                .then(buffer => {
-                    // ここでbufferを使用します。bufferは、変換後の画像データを含んでいます。
-                    outputBuffer = buffer;
-                })
-                .catch(err => {
-                    console.error(err);
-                });
         } else {
-            outputBuffer = await sharp(output2Buffer).resize(width, height).toBuffer();
+            if (hanten == "true") {
+                if (yokohanten == "true") {
+                outputBuffer = await sharp(output2Buffer).resize(width, height).blur(bokasi).rotate(rotate).flop().toBuffer();
+                }else{
+                    outputBuffer = await sharp(output2Buffer).resize(width, height).blur(bokasi).rotate(rotate).toBuffer();
+                }
+            } else { if (yokohanten == "true") {outputBuffer = await sharp(output2Buffer).resize(width, height).blur(bokasi).rotate(rotate).flip().flop().toBuffer();}else{outputBuffer = await sharp(output2Buffer).resize(width, height).blur(bokasi).rotate(rotate).flip().toBuffer();} }
         }
         let base64Image = outputBuffer.toString('base64');
         const pic = `data:image/${mimeType};base64,${base64Image}`;
@@ -159,7 +232,7 @@ app.post('/post.html', upload.single('file'), async function (request, response)
     }
 
     if (request.body["kakutyousi"] in mimeTypes) {
-        await convertAndSendFile(request.body["kakutyousi"], Number(request.body["quality"]), Number(request.body["width"]), Number(request.body["height"]), request.body["resize"]);
+        await convertAndSendFile(request.body["kakutyousi"], Number(request.body["quality"]), Number(request.body["width"]), Number(request.body["height"]), request.body["resize"], Number(request.body["rotate"]), request.body["hanten"], request.body["yokohanten"],Number(request.body["bokasi"]));
     }
 
 });
